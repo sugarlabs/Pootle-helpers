919fb55c34d086782a49d6d53bc78bbbe6b01071
cjl <cjlhomeaddress@gmail.com>
2012-01-30 11:09:15 -0500

    modify TamTam pack generation statement

--- langpackgen/src/LangPackGen/pack_generator.py
+++ langpackgen/src/LangPackGen/pack_generator.py
@@ -10,14 +10,14 @@ import shutil
 
 import translate.storage.po
 
-LANGS = ['af', 'am', 'ar', 'aym', 'bg', 'bn', 'ca', 'da', 'de', 'el', 'en_GB', 
-         'es', 'fa', 'fa_AF', 'fil', 'fr', 'hi', 'ht', 'hus', 'hy', 'id', 'it', 'ja',
-         'km', 'mg', 'mk', 'mn', 'mr', 'nb', 'ne', 'nl', 'pap', 'pl', 'ps',
-         'pt', 'pt_BR', 'quy', 'quz', 'ro', 'ru', 'rw', 'si', 'sl', 'sq', 'sv',
-         'sw', 'ta', 'te', 'th', 'tr', 'ur', 'vi', 'zh_CN', 'zh_TW'
-         ]
-TRANSLATE_DIR = '/var/lib/pootle/translations'
+LANGS = ['af', 'am', 'ar', 'ay', 'bg', 'bn', 'bn_IN', 'ca', 'de', 'dz', 'el', 
+         'en', 'es', 'fa', 'fa_AF', 'ff', 'fr', 'gu', 'ha', 'hi', 'ht', 'hy', 'ig', 
+         'is', 'it', 'ja', 'km', 'ko', 'mg', 'mk', 'ml', 'mn', 'mr', 'mvo', 'nb', 
+         'ne', 'nl', 'pa', 'pap', 'pis', 'pl', 'ps', 'pt', 'pt_BR', 
+         'qu', 'ro', 'ru', 'rw', 'sd', 'si', 'sl', 'ta', 'te', 'th', 
+         'tpi', 'tr', 'ur', 'vi', 'yo', 'zh_CN', 'zh_TW']
 
+TRANSLATE_DIR = '/var/lib/pootle/translations'
 
 def gen_langpack(lang, tmpdir, configfile, opdir):
     
@@ -67,10 +67,12 @@ def gen_langpack(lang, tmpdir, configfile, opdir):
         for molocation in molocations:
             f.write('\nif [[ -d ' + os.path.dirname(molocation[:(molocation.find('LL') - 1)]) + ' ]] ; then')
             f.write(install_string + name.replace(' ', '\\ ') + '.mo ' + molocation.replace('LL', lang))
+            f.write('\nfi\n')
             
         if needs_linfo == 1:
+            f.write('\nif [[ -d ' + os.path.dirname(molocation[:(molocation.find('LL') - 1)]) + ' ]] ; then')
             f.write(install_string + name.replace(' ', '\\ ') + '.linfo ' + linfo)
-        f.write('\nfi\n')
+            f.write('\nfi\n')
         
     f.write ('\n\n\ncp uninstall_langpack /usr/bin/uninstall_langpack_' + lang)
 
